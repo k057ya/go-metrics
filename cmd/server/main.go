a6614578521f17c13b5f84881acb65c298c17a99
k057ya <321773357+k057ya@users.noreply.github.com>
2026-09-17 16:03:47 +0300

    Iteration 6: Server request logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/k057ya/go-metrics/internal/config"
 	"github.com/k057ya/go-metrics/internal/handler"
+	"github.com/k057ya/go-metrics/internal/logger"
+	"github.com/k057ya/go-metrics/internal/middleware"
 	"github.com/k057ya/go-metrics/internal/repository"
 )
 
@@ -20,6 +22,10 @@ type EnvConfig struct {
 func main() {
 
 	storage := repository.NewMemStorage()
+	if err := logger.Initialize("info"); err != nil {
+		fmt.Println("Error while initializing logger " + err.Error())
+		return
+	}
 
 	flag.Var(config.ServerConfig, "a", "Server host and port")
 	flag.Parse()
@@ -37,6 +43,8 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+	// Add logger middleware for all routes
+	router.Use(middleware.Log)
 	// List all metrics
 	router.Get("/", func(w http.ResponseWriter, req *http.Request) {
 		handler.ListAllMetrics(w, req, storage)
